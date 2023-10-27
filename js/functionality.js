5ceb2e5d0b71abbee0c39a9391ae3648e1dfba5b
Sarah <sarahblok@hotmail.com>
2023-10-27 15:53:52 +0200

    updated login and sign up

--- js/functionality.js
+++ js/functionality.js
@@ -1,9 +1,33 @@
-// Reset Password Button
 document.addEventListener("DOMContentLoaded", function () {
-    const resetPasswordButton = document.getElementById("resetPasswordButton");
-    resetPasswordButton.addEventListener("click", function () {
-      // Show the notification message when the button is clicked
-      document.getElementById("notification-message").style.display = "block";
-    });
+  const loginForm = document.getElementById("login-form");
+  const resetPasswordButton = document.getElementById("resetPasswordButton");
+  const notificationMessage = document.getElementById("notification-message");
+  const successMessage = document.getElementById("success-message");
+  const errorAlert = document.getElementById("error-alert");
+
+  // Handle form submission
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // TODO: Implement your login logic here
+    if (email === "your@email.com" && password === "yourpassword") {
+      // If login is successful, show success message
+      successMessage.style.display = "block";
+      errorAlert.style.display = "none";
+    } else {
+      // Show an error message for incorrect login
+      errorAlert.style.display = "block";
+      successMessage.style.display = "none";
+    }
   });
-  
+
+  // Handle reset password button click
+  resetPasswordButton.addEventListener("click", function () {
+    // TODO: Implement your reset password logic here
+
+    // Show a notification message
+    notificationMessage.style.display = "block";
+  });
+});
