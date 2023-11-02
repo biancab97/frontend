43cbdb715bb6e9c6459db69b1d25df71c4c0ff7a
Martin <martin_de_v@hotmail.com>
2023-11-02 09:24:48 +0100

    opdrachtgeverpagina

--- js/traineecrud.js
+++ js/traineecrud.js
@@ -33,7 +33,6 @@ function displayTraineeInfo(trainee) {
     <div class="row">
         <div class="col-md-6">
             <h3 class="text-primary">${trainee.naam}</h3>
-            <p class="card-text"><strong>Wachtwoord:</strong> ${trainee.wachtwoord}</p>
             <p class="card-text"><strong>E-mail:</strong> ${trainee.email}</p>
             <p class="card-text"><strong>Telefoon:</strong> ${trainee.telefoon}</p>
             <p class="card-text"><strong>Richting:</strong> ${trainee.richting}</p>
@@ -44,7 +43,6 @@ function displayTraineeInfo(trainee) {
             <img src="../img/${trainee.foto}" class="img-fluid">
         </div>
     </div>
-    
         `;
 }
 
