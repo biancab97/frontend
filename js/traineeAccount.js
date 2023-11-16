cb3a1a168661c759ec6eb4fce8ac6d73ab300ead
Sarah <sarahblok@hotmail.com>
2023-11-16 13:56:30 +0100

    update account

--- js/traineeAccount.js
+++ js/traineeAccount.js
@@ -1,7 +1,6 @@
-document.addEventListener("DOMContentLoaded", async function () {
-	// Set the traineeId to any specific ID you want to retrieve.
-	const traineeId = 25;
+let traineeId = localStorage.getItem("id");
 
+document.addEventListener("DOMContentLoaded", async function () {
 	try {
 		const trainee = await getTraineeById(traineeId);
 		populateFields(trainee);
@@ -16,24 +15,32 @@ function populateFields(trainee) {
 	document.getElementById("wachtwoordInput").value = trainee.wachtwoord;
 	document.getElementById("emailInput").value = trainee.email;
 	document.getElementById("telefoonnummerInput").value = trainee.telefoon;
-	document.getElementById("uitstroomrichtingInput").value = trainee.richting;
+	document.getElementById("uitstroomDropdown").value = trainee.richting;
 	document.getElementById("motivatieInput").value = trainee.motivatie;
 	document.getElementById("bioInput").value = trainee.bio;
 	document.getElementById("woonplaatsInput").value = trainee.woonplaats;
-	// document.getElementById("fotoInput").value = trainee.foto;
-	// document.getElementById("cvInput").value = trainee.cv;
-
-	// Event listener for the submit button
-	setupEditSaveListeners("submitBtn", trainee);
+	document.getElementById("fotoInput").src = trainee.foto;
+	document.getElementById("cvInput").value = trainee.cv;
 }
 
-function updateTrainee() {
+function putTrainee() {
+	let updatedTrainee = {
+		naam: document.getElementById("nameInput").value,
+		wachtwoord: document.getElementById("wachtwoordInput").value,
+		email: document.getElementById("emailInput").value,
+		telefoon: document.getElementById("telefoonnummerInput").value,
+		richting: document.getElementById("uitstroomDropdown").value,
+		motivatie: document.getElementById("motivatieInput").value,
+		bio: document.getElementById("bioInput").value,
+		woonplaats: document.getElementById("woonplaatsInput").value,
+		cv: document.getElementById("cvInput").value,
+	};
+	// foto: document.getElementById("fotoInput").src
+
 	// Update the trainee
 	try {
-		// await updateTrainee(trainee.id, updatedData);
-
-		populateFields(updatedTrainee);
+		updateTrainee(traineeId, updatedTrainee);
 	} catch (error) {
-		console.error(`Error updating ${fieldId}:`, error);
+		console.error(`Error updating`, error);
 	}
 }
