42f109b1ad5bd2011e16481d03f526829953aff1
amarborz <amarborz>
2023-11-22 11:20:41 +0100

    added new backend path

--- js/navBar.js
+++ js/navBar.js
@@ -1,4 +1,5 @@
-const backendPath = 'https://yc2310-match-backend.azurewebsites.net/'
+const backendPath = 'https://match-backend.azurewebsites.net/'
+// const backendPath = 'https://yc2310-match-backend.azurewebsites.net/'
 // const backendPath = 'http://localhost:8080/'
 
 const profile = localStorage.getItem('role')
