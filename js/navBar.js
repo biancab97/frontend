2ef3d95259c3e85306e3916e2fa5390489df396f
amarborz <amarborz>
2023-11-22 15:06:16 +0100

    fixed navbar

--- js/navBar.js
+++ js/navBar.js
@@ -5,68 +5,65 @@ const backendPath = 'https://match-backend.azurewebsites.net/'
 const profile = localStorage.getItem('role')
 
 const loadNavBar = () => {
-  const navbarContainer = document.createElement('nav')
+	const navbarContainer = document.createElement('nav')
 
-  navbarContainer.classList.add(
-    'navbar',
-    'navbar-expand-lg',
-    'navbar-light',
-    'bg-primary',
-    'fixed-top'
-  )
+	navbarContainer.classList.add(
+		'navbar',
+		'navbar-expand-lg',
+		'navbar-light',
+		'bg-primary',
+		'fixed-top'
+	)
 
-  // Define the HTML code for the navigation bar
+	// Define the HTML code for the navigation bar
 
-  let url = window.location.href
-  console.log("profiel: ", profile)
-  if (profile === 'ROLE_OPDRACHTGEVER') {
-    console.log('Opdrachtgever')
-    navbarContainer.innerHTML = `
+	let url = window.location.href
+	console.log('profiel: ', profile)
+	if (profile === 'ROLE_OPDRACHTGEVER') {
+		console.log('Opdrachtgever')
+		navbarContainer.innerHTML = `
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
-      <a class="navbar-brand" href="homeOpdracht.html">
-        <img
-          src="../icons/logo.png"
-          width="100"
-          height="30"
-          class="d-inline-block align-top mx-3"
-          alt="Your Logo"
-        />
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="accountOpdrachtgever.html">Mijn Account</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="homeOpdracht.html">Vacatures</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="OpdrachtgeverMatches.html">Matches</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="OpdrachtgeverMatches.html">Opdrachten</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="loonstrook.html">Loonstroken</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="berichten.html">Berichten</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <a class="navbar-brand" href="homeOpdracht.html">
+      <img
+        src="../icons/logo.png"
+        width="100"
+        height="30"
+        class="d-inline-block align-top mx-3"
+        alt="Your Logo"
+      />
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="accountOpdrachtgever.html">Mijn Account</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="homeOpdracht.html">Vacatures</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="OpdrachtgeverMatches.html">Matches</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="https://dataproject.azurewebsites.net/leeftijd" target="_blank">Statistieken</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="berichten.html">Berichten</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   
     <style>
       .navbar-nav .nav-item:hover {
@@ -75,9 +72,9 @@ const loadNavBar = () => {
     </style>
 
     `
-  } else if (profile === 'ROLE_TRAINEE') {
-    console.log('Trainee')
-    navbarContainer.innerHTML = `
+	} else if (profile === 'ROLE_TRAINEE') {
+		console.log('Trainee')
+		navbarContainer.innerHTML = `
   <!-- Navigation Bar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
     <a class="navbar-brand" href="homeTrainees.html">
@@ -131,9 +128,9 @@ const loadNavBar = () => {
     }
   </style>
   `
-  } else if (profile === "ROLE_TALENTMANAGER") {
-    console.log('Talent')
-    navbarContainer.innerHTML = `
+	} else if (profile === 'ROLE_TALENTMANAGER') {
+		console.log('Talent')
+		navbarContainer.innerHTML = `
   <!-- Navigation Bar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
     <a class="navbar-brand" href="homeTalent.html">
@@ -184,8 +181,8 @@ const loadNavBar = () => {
     }
   </style>
   `
-  }
+	}
 
-  // Append the navigation bar to the document
-  document.body.insertBefore(navbarContainer, document.body.firstChild)
+	// Append the navigation bar to the document
+	document.body.insertBefore(navbarContainer, document.body.firstChild)
 }
